e4f47006a962046e44b4dfbbe03ac1b6ed452613
warfarm <67772410+warfarm@users.noreply.github.com>
2024-08-26 22:01:17 -0400

    Add Bounce Around

--- src/main.c
+++ src/main.c
@@ -4,6 +4,10 @@
 #define ENABLE_DEBUG
 
 #define MAX_WINDOW_TITLE_LEN 64
+
+#define SPEED_RANDOMNESS 30
+#define BASE_SPEED 120
+
 /*
  * geta list of windows
  * map each one to a number value
@@ -17,6 +21,8 @@ typedef struct WindowInfo {
 
 // Globals
 unsigned int found_win_count = 0;
+unsigned int screen_w;
+unsigned int screen_h;
 int max_win_count = 32;
 
 // EnumWindows accepts a callback function to run on each window found:
@@ -24,6 +30,8 @@ BOOL window_callback(HWND hwnd, LPARAM lParam);
 
 int main(int argc, char *argv[]) 
 {
+    screen_w = GetSystemMetrics(SM_CXSCREEN);
+    screen_h = GetSystemMetrics(SM_CYSCREEN);
     utils_init();
     printf("Enter window scan limit: ");
     scanf("%d", &max_win_count);
@@ -78,10 +86,12 @@ int main(int argc, char *argv[])
     // What are we doing chat
     double old_time = get_absolute_time();
     
+    float speed_variance = rand() % SPEED_RANDOMNESS + BASE_SPEED;
+
     Vec2 vel = {
         // 20-40 random chance
-        .x = rand() % 20 + 20,
-        .y = rand() % 20 + 20
+        .x = speed_variance,
+        .y = SPEED_RANDOMNESS - speed_variance
     };
     FRect win_rect = frect_from_winrect(window_rect);
 
@@ -98,15 +108,77 @@ int main(int argc, char *argv[])
         // printf("deltatime: %lf\n", dt);
         
         // move window places
-        
+        float new_speed_variance = rand() % SPEED_RANDOMNESS + BASE_SPEED;
+
         // kinematicm oment
         float dx = vel.x * dt;
         float dy = vel.y * dt;
         win_rect.top_left.x += dx;
         win_rect.top_left.y += dy;
 
-        // TODO! CHECK IF lower bound of window is greater than resolution
-        
+        if (win_rect.top_left.x <= 0)
+        {
+            win_rect.top_left.x = 0;
+
+            vel.x = new_speed_variance;
+            vel.y = SPEED_RANDOMNESS - new_speed_variance;
+
+            vel.x *= -1;
+        }
+
+        if (win_rect.top_left.x + win_rect.w >= screen_w){
+            win_rect.top_left.x = screen_w - win_rect.w;
+
+            vel.x = new_speed_variance;
+            vel.y = SPEED_RANDOMNESS - new_speed_variance;
+
+            vel.x *= -1;
+        }
+
+        if (win_rect.top_left.y <= 0)
+        {
+            win_rect.top_left.y = 0;
+
+            vel.x = new_speed_variance;
+            vel.y = SPEED_RANDOMNESS - new_speed_variance;
+
+            vel.y *= -1;
+        }
+
+        if (win_rect.top_left.y + win_rect.h >= screen_h){
+            win_rect.top_left.y = screen_h - win_rect.h;
+
+            vel.x = new_speed_variance;
+            vel.y = SPEED_RANDOMNESS - new_speed_variance;
+
+            vel.y *= -1;
+        }
+
+        // TODO! CHECK IF bounds of window are greater or equal to resolution
+        // if (win_rect.top_left.x <= 0 ||                         // Checks if the top left of the screen does not go past the left
+        //     win_rect.top_left.x + win_rect.w >= screen_w)     // Checks if the top right goes past the screen width
+        // {
+            
+        //     #ifdef ENABLE_DEBUG
+        //             printf("CONTACT Y AXIS");
+        //     #endif
+
+        //     
+
+        // }
+
+        // if (win_rect.top_left.y <= 0 ||
+        //     win_rect.top_left.y + win_rect.h >= screen_h)
+        // {
+            
+        //     #ifdef ENABLE_DEBUG
+        //             printf("CONTACT Y AXIS");
+        //     #endif
+            
+        //     vel.y *= -1;
+
+        // }
+
         SetWindowPos(window->handle, NULL, win_rect.top_left.x, win_rect.top_left.y, win_rect.w, win_rect.h, 0);
     }
 
